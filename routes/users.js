990eea2d1c199b88b619ffc148c34f63226e4b04
Chengyu Sun <csun@calstatela.edu>
2018-04-24 22:08:12 -0700

    Implemented a controller and a view

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find( (err, users) => {
+     res.render('users', {title: 'Users', users: users});
+  });
 });
 
 module.exports = router;
